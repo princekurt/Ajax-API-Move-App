47e358db6af5fc8fa153ca81a8b2dfcdd28af03e
Kurt <kurtanderson234@gmail.com>
2017-05-31 09:16:24 -0500

    Added Wikipedia API

--- js/script.js
+++ js/script.js
@@ -15,6 +15,8 @@ function loadData() {
     var $street = $('#street').val();
     var $city = $('#city').val();
     console.log($city);
+	
+	//Goggle API
     var src = ("http://maps.googleapis.com/maps/api/streetview?size=600x400&location=" + $street + ", " +  $city + '').toString();
     console.log(src);
 
@@ -23,7 +25,7 @@ function loadData() {
     $greeting.text('So, you want to live at ' + $street + ", " + $city + "?");
     $body.append('<img class ="bgimg" src="' + src + '">');
 	
-	
+	//NYT API
 	var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 	url += '?' + $.param({
 	'api-key': "74259929d1e34e3d99a51280b4e005d3",
@@ -42,6 +44,30 @@ function loadData() {
 	  };
 	});
 	
+	//WikiPedia API
+	var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + $street +
+		'&format=json&callback=wikiCallback';
+	
+	var wikiRequestTimeout = setTimeout(function(){
+		$wikiElem.text("failed to get wikipedia resources");
+	}, 8000);
+	
+	$.ajax({
+		url:wikiUrl,
+		dataType: "jsonp",
+		success: function(response) {
+			var articleList = response[1];
+			
+			for (var i = 0; i <articleList.length; i++) {
+				console.log(articleList[i];
+				articleStr = articleList[i];
+				var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+				$wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
+			};
+			
+			clearTimeout(wikiRequestTimeout);
+		}
+	});
     return false;
 };
 
